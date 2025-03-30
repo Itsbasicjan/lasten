32d983dfb065cd5b97d76be6cbf3fd25c3af5e9c
itsbasicjan <jandeluxe96@gmail.com>
2025-03-30 20:37:48 +0200

    ohyh

---
+++ README.md
@@ -0,0 +1,30 @@
+# meinplugin
+
+A short description of the project
+
+## Installation
+
+### InvenTree Plugin Manager
+
+... todo ...
+
+### Command Line 
+
+To install manually via the command line, run the following command:
+
+```bash
+pip install meinplugin
+```
+
+## Configuration
+
+... todo ...
+
+## Usage
+
+... todo ...
+# inventree_plugin
+# inventree
+# inventree
+# inventree
+# lasten
---
+++ meinplugin/core.py
@@ -0,0 +1,116 @@
+"""A short description of the project"""
+
+# Standard Django imports for Views and URLs
+from django.http import HttpResponse
+from django.urls import path
+
+# InvenTree plugin imports
+from plugin import InvenTreePlugin
+from plugin.mixins import (NavigationMixin, ScheduleMixin, SettingsMixin,
+                           UrlsMixin, UserInterfaceMixin)
+
+from . import PLUGIN_VERSION # Stellt sicher, dass die Version aus __init__.py geladen wird
+
+# Die Hauptklasse für dein Plugin
+class meinplugin(ScheduleMixin, SettingsMixin, UserInterfaceMixin, NavigationMixin, UrlsMixin, InvenTreePlugin):
+    """meinplugin - custom InvenTree plugin."""
+
+    # Plugin metadata
+    TITLE = "Mein Cooles Plugin" # Angepasster Titel
+    NAME = "meinplugin"
+    SLUG = "meinplugin" # Wichtig für URL-Referenzen!
+    DESCRIPTION = "Ein Beispiel-Plugin mit Navigationselement"
+    VERSION = PLUGIN_VERSION
+
+    # Additional project information
+    AUTHOR = "Jan Schüler"
+    LICENSE = "MIT"
+
+    # Optional: Gruppiere die Links unter einem eigenen Tab in der Navigation
+    NAVIGATION_TAB_NAME = "Mein Plugin Tab"
+    NAVIGATION_TAB_ICON = 'fas fa-cogs' # Beispiel FontAwesome 4 Icon
+
+    # Navigationselemente (von NavigationMixin)
+    # Ref: https://docs.inventree.org/en/latest/extend/plugins/integration/navigation/
+    NAVIGATION = [
+        {
+            'name': 'Beispiel Seite', # Angezeigter Text des Links
+            'link': 'plugin:meinplugin:hello', # URL-Name: plugin:<SLUG>:<url_name>
+            'icon': 'fas fa-info-circle', # Beispiel FontAwesome 4 Icon
+        },
+        # Hier könnten weitere Links für dieses Plugin hinzugefügt werden
+    ]
+
+    def setup_urls(self):
+    """Definiert URL-Muster für dieses Plugin."""
+    return [
+        # Definiert eine URL unter /plugin/meinplugin/hello/
+        path('hello/', self.hello_world_view, name='hello'),
+        # URL für die Ausleihfunktion: /plugin/meinplugin/ausleihen/
+        path('ausleihen/', self.loan_view, name='ausleihen'),
+    ]
+
+    # Eine einfache View-Funktion, die einen "Hallo Welt"-Text ausgibt
+    def hello_world_view(self, request, *args, **kwargs):
+        """Eine Beispiel-Ansicht, die von der URL aufgerufen wird."""
+        html = "<h1>Hallo Welt!</h1><p>Dies ist eine einfache Seite vom 'meinplugin'.</p>"
+        return HttpResponse(html)
+
+    # Die neue View-Funktion für die Ausleihe
+    def loan_view(self, request, *args, **kwargs):
+        """Ausleihfunktion in InvenTree.
+        
+        Zeigt ein Formular an, über das ein Artikel ausgeliehen werden kann.
+        Bei POST wird die Anfrage verarbeitet und eine Bestätigung angezeigt.
+        """
+        if request.method == 'POST':
+            # Formular-Daten aus dem POST-Request auslesen
+            artikel_id = request.POST.get('artikel')
+            benutzer = request.POST.get('benutzer')
+            
+            # Hier die Logik zur Ausleihe implementieren:
+            # z. B. Status des Artikels aktualisieren, einen Ausleih-Datensatz anlegen, etc.
+            # Dies ist ein Beispiel, das lediglich eine Bestätigungsmeldung anzeigt.
+            html = f"<h1>Ausleihe bestätigt!</h1><p>Artikel {artikel_id} wurde an {benutzer} ausgeliehen.</p>"
+            return HttpResponse(html)
+        else:
+            # GET-Request: Zeige das Ausleihformular an
+            html = """
+                <h1>Artikel ausleihen</h1>
+                <form method="post">
+                    <label for="artikel">Artikel-ID:</label>
+                    <input type="text" id="artikel" name="artikel"><br><br>
+                    <label for="benutzer">Benutzername:</label>
+                    <input type="text" id="benutzer" name="benutzer"><br><br>
+                    <input type="submit" value="Ausleihen">
+                </form>
+            """
+            return HttpResponse(html)
+
+
+    # --- Bestehende Mixin-Konfigurationen ---
+
+    # Scheduled tasks (from ScheduleMixin)
+    # Ref: https://docs.inventree.org/en/stable/extend/plugins/schedule/
+    SCHEDULED_TASKS = {
+        # Define your scheduled tasks here...
+    }
+
+    # Plugin settings (from SettingsMixin)
+    # Ref: https://docs.inventree.org/en/stable/extend/plugins/settings/
+    SETTINGS = {
+        'CUSTOM_VALUE': {
+            'name': 'Custom Value',
+            'description': 'A custom value',
+            'validator': int,
+            'default': 42,
+        }
+        # Hier könnten weitere Einstellungen definiert werden
+    }
+
+    # User interface elements (from UserInterfaceMixin)
+    # Ref: https://docs.inventree.org/en/stable/extend/plugins/ui/
+    # Diese sind für Dashboard-Elemente, Panels etc. relevant,
+    # nicht direkt für die einfache Navigation, die wir hier erstellen.
+    # Die bereitgestellten JavaScript-Beispiele würden hier konfiguriert werden,
+    # wenn du z.B. ein benutzerdefiniertes Panel hinzufügen wolltest.
